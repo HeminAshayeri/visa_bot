30d6fb1dc57fbc309c8c277b66759355115d55ca
HeminAshayeri <Hemin.Ashayeri@Gmail.com>
2026-02-06 16:27:22 +0330

    Update visa_bot.py

--- visa_bot.py
+++ visa_bot.py
@@ -66,4 +66,5 @@ bot.set_webhook(url=URL)
 
 # Start Flask server
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
