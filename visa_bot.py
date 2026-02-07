ddcc83c182306151fc6cf3ffc245db5f99dca489
Hemin <Hemin.Ashayeri@Gmail.com>
2026-02-07 14:07:32 +0330

    Update visa_bot.py

--- visa_bot.py
+++ visa_bot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(token=TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    welcome_text = f"user {message.from_user.first_name} Welcome to our bot!"
+    welcome_text = f"Dear {message.from_user.first_name} Welcome to our bot!"
     bot.send_message(chat_id=message.chat.id, text=welcome_text)
 
 @bot.message_handler(func = lambda message : True, 
@@ -84,6 +84,7 @@ bot.set_webhook(url=URL)
 # Start Flask server
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
 
 
